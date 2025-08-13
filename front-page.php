107ca8a77fb856dd4a7f1c4ecf5db5d4979652b0
kaizen-design <denis@kaizen-design.ru>
2025-08-13 22:26:48 +0300

    Services section created

--- front-page.php
+++ front-page.php
@@ -12,6 +12,7 @@ get_header(); ?>
   <?php get_template_part( 'template-parts/section', 'marquee' ); ?>
   <?php get_template_part( 'template-parts/section', 'masonry' ); ?>
   <?php get_template_part( 'template-parts/section', 'slider' ); ?>
+  <?php get_template_part( 'template-parts/section', 'services' ); ?>
 </main>
 
 <?php get_footer();
---
+++ template-parts/section-services.php
@@ -0,0 +1,33 @@
+<section class="services-section" data-aos="fade-up">
+  <div class="container-xl">
+    <div class="services-box d-flex flex-column">
+      <h2 class="section-title text-uppercase mb-0 text-center position-relative d-flex justify-content-center">
+        <span class="position-relative">Our Services</span>
+      </h2>
+      <div class="row">
+        <div class="col-xl-2 offset-xl-1" data-aos="fade-right" data-aos-delay="300">
+          <img 
+            src="<?= get_template_directory_uri() ?>/assets/images/bird.webp" 
+            class="img-fluid" 
+            loading="lazy"
+            alt="Our Services"
+          />
+        </div>
+        <div class="col-xl-9 section-content d-flex flex-column">
+          <p class="mb-0" data-aos="fade-left" data-aos-delay="300">Zoom Wedding Studio offers you a full package of wedding services to&nbsp;create the best memories on your special day.</p>
+          <ul class="services-list list-unstyled mb-0 d-flex align-items-center">
+            <li data-aos="fade-left" data-aos-delay="300">Photography</li>
+            <li data-aos="fade-left" data-aos-delay="600">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect fill="none" height="256" width="256"/><path d="M176,32a60,60,0,0,0-48,24A60,60,0,0,0,20,92c0,71.9,99.9,128.6,104.1,131a7.8,7.8,0,0,0,3.9,1,7.6,7.6,0,0,0,3.9-1,314.3,314.3,0,0,0,51.5-37.6C218.3,154,236,122.6,236,92A60,60,0,0,0,176,32Z"/></svg>
+            </li>
+            <li data-aos="fade-left" data-aos-delay="900">Videography</li>
+            <li data-aos="fade-left" data-aos-delay="1200">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect fill="none" height="256" width="256"/><path d="M176,32a60,60,0,0,0-48,24A60,60,0,0,0,20,92c0,71.9,99.9,128.6,104.1,131a7.8,7.8,0,0,0,3.9,1,7.6,7.6,0,0,0,3.9-1,314.3,314.3,0,0,0,51.5-37.6C218.3,154,236,122.6,236,92A60,60,0,0,0,176,32Z"/></svg>
+            </li>
+            <li data-aos="fade-left" data-aos-delay="1500">Wedding Album</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
