16732b826b6368b1b6247a1286782f35770c76d5
kaizen-design <denis@kaizen-design.ru>
2025-08-14 11:57:13 +0300

    AOS animations

--- template-parts/section-services.php
+++ template-parts/section-services.php
@@ -5,7 +5,7 @@
         <span class="position-relative">Our Services</span>
       </h2>
       <div class="row">
-        <div class="col-xl-2 offset-xl-1" data-aos="fade-right" data-aos-delay="300">
+        <div class="col-xl-2 offset-xl-1" data-aos="fade-right" data-aos-delay="300" data-aos-anchor-placement="top-bottom">
           <img 
             src="<?= get_template_directory_uri() ?>/assets/images/bird.webp" 
             class="img-fluid" 
@@ -14,17 +14,17 @@
           />
         </div>
         <div class="col-xl-9 section-content d-flex flex-column">
-          <p class="mb-0" data-aos="fade-left" data-aos-delay="300">Zoom Wedding Studio offers you a full package of wedding services to&nbsp;create the best memories on your special day.</p>
+          <p class="mb-0" data-aos="fade-left" data-aos-delay="300" data-aos-anchor-placement="top-bottom">Zoom Wedding Studio offers you a full package of wedding services to&nbsp;create the best memories on your special day.</p>
           <ul class="services-list list-unstyled mb-0 d-flex align-items-center">
-            <li data-aos="fade-left" data-aos-delay="300">Photography</li>
-            <li data-aos="fade-left" data-aos-delay="600">
+            <li data-aos="fade-left" data-aos-delay="300" data-aos-anchor-placement="top-bottom">Photography</li>
+            <li data-aos="fade-left" data-aos-delay="600" data-aos-anchor-placement="top-bottom">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect fill="none" height="256" width="256"/><path d="M176,32a60,60,0,0,0-48,24A60,60,0,0,0,20,92c0,71.9,99.9,128.6,104.1,131a7.8,7.8,0,0,0,3.9,1,7.6,7.6,0,0,0,3.9-1,314.3,314.3,0,0,0,51.5-37.6C218.3,154,236,122.6,236,92A60,60,0,0,0,176,32Z"/></svg>
             </li>
-            <li data-aos="fade-left" data-aos-delay="900">Videography</li>
-            <li data-aos="fade-left" data-aos-delay="1200">
+            <li data-aos="fade-left" data-aos-delay="900" data-aos-anchor-placement="top-bottom">Videography</li>
+            <li data-aos="fade-left" data-aos-delay="1200" data-aos-anchor-placement="top-bottom">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect fill="none" height="256" width="256"/><path d="M176,32a60,60,0,0,0-48,24A60,60,0,0,0,20,92c0,71.9,99.9,128.6,104.1,131a7.8,7.8,0,0,0,3.9,1,7.6,7.6,0,0,0,3.9-1,314.3,314.3,0,0,0,51.5-37.6C218.3,154,236,122.6,236,92A60,60,0,0,0,176,32Z"/></svg>
             </li>
-            <li data-aos="fade-left" data-aos-delay="1500">Wedding Album</li>
+            <li data-aos="fade-left" data-aos-delay="1500" data-aos-anchor-placement="top-bottom">Wedding Album</li>
           </ul>
         </div>
       </div>
--- template-parts/section-slider.php
+++ template-parts/section-slider.php
@@ -48,14 +48,14 @@
             </div>
           </div>
           <div class="slider-footer d-flex align-items-center">
-            <a href="#" class="btn btn-primary btn-lg d-flex align-items-center me-auto">
+            <a href="#" class="btn btn-primary btn-lg d-flex align-items-center me-auto" data-aos="fade-right" data-aos-anchor-placement="top-bottom">
               View all
               <svg class="svg-arrow-right" width="15" height="15" viewBox="0 0 24 24" fill="none">
                 <path d="M16.3153 16.6681C15.9247 17.0587 15.9247 17.6918 16.3153 18.0824C16.7058 18.4729 17.339 18.4729 17.7295 18.0824L22.3951 13.4168C23.1761 12.6357 23.1761 11.3694 22.3951 10.5883L17.7266 5.9199C17.3361 5.52938 16.703 5.52938 16.3124 5.91991C15.9219 6.31043 15.9219 6.9436 16.3124 7.33412L19.9785 11.0002L2 11.0002C1.44772 11.0002 1 11.4479 1 12.0002C1 12.5524 1.44772 13.0002 2 13.0002L19.9832 13.0002L16.3153 16.6681Z" fill="#0F0F0F"/>
               </svg>
             </a>
-            <span class="slides-counter">01 / <span class="total">25</span></span>
-            <ul class="slider-controls list-unstyled mb-0 d-flex align-items-center">
+            <span class="slides-counter" data-aos="fade-left" data-aos-anchor-placement="top-bottom">01 / <span class="total">25</span></span>
+            <ul class="slider-controls list-unstyled mb-0 d-flex align-items-center" data-aos="fade-left" data-aos-anchor-placement="top-bottom">
               <li>
                 <a href="#" class="slide-prev">
                   <svg fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24"><polyline points="15 18 9 12 15 6"/></svg>
