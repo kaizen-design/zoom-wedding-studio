3658bfed9ba8856c633a98e01e4ea7f345b89a7e
kaizen-design <denis@kaizen-design.ru>
2025-09-11 14:20:18 +0300

    Project package section completed

--- single-projects.php
+++ single-projects.php
@@ -4,6 +4,9 @@ get_header();
 
 <main>
   <?php get_template_part( 'template-parts/project', 'hero' ); ?>
+  <?php get_template_part( 'template-parts/project', 'description' ); ?>
+  <?php get_template_part( 'template-parts/project', 'video' ); ?>
+  <?php get_template_part( 'template-parts/project', 'package' ); ?>
   <?php get_template_part( 'template-parts/section', 'masonry' ); ?>
   <?php get_template_part( 'template-parts/section', 'contact' ); ?>
   <?php get_template_part( 'template-parts/section', 'instagram' ); ?>
