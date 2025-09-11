e482d0a7da1489f96f9a9d0d57f2d197b290ac0b
kaizen-design <denis@kaizen-design.ru>
2025-09-11 15:25:50 +0300

    Project reels section created

--- single-projects.php
+++ single-projects.php
@@ -7,6 +7,7 @@ get_header();
   <?php get_template_part( 'template-parts/project', 'description' ); ?>
   <?php get_template_part( 'template-parts/project', 'video' ); ?>
   <?php get_template_part( 'template-parts/project', 'package' ); ?>
+  <?php get_template_part( 'template-parts/project', 'reels' ); ?>
   <?php get_template_part( 'template-parts/section', 'masonry' ); ?>
   <?php get_template_part( 'template-parts/section', 'contact' ); ?>
   <?php get_template_part( 'template-parts/section', 'instagram' ); ?>
---
+++ template-parts/project-reels.php
@@ -0,0 +1,44 @@
+<section class="project-reels-section">
+  <div class="container-xl">
+    <div class="row justify-content-between">
+      <div class="section-content col-xl-4 d-flex flex-column">
+        <h2 class="section-title mb-0 d-flex flex-column">
+          <span class="section-subtitle text-uppercase" data-aos="fade-right">Instagram Reels</span>  
+          <span data-aos="fade-right" data-aos-delay="150">Stephanie and Eric's love stories for Instagram</span>
+        </h2>
+        <p class="mb-0" data-aos="fade-left" data-aos-delay="300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      <div class="image-section col-xl-7">
+        <div class="row align-items-end gx-6">
+          <div class="col-lg-6">
+            <div class="section-img" data-aos="fade-up" data-aos-delay="300">
+              <img 
+                src="<?= get_template_directory_uri() ?>/assets/tmp/reels-1.webp" 
+                alt="Stephanie & Eric" 
+                class="object-fit-cover img-fluid w-100"
+                width="300"
+                height="530"
+                loading="lazy"
+              />
+            </div>
+          </div>
+          <div class="col-lg-6 d-flex flex-column">
+            <div class="section-img" data-aos="fade-up">
+              <img 
+                src="<?= get_template_directory_uri() ?>/assets/tmp/reels-2.webp" 
+                alt="Stephanie & Eric" 
+                class="object-fit-cover img-fluid w-100"
+                width="300"
+                height="530"
+                loading="lazy"
+              />
+            </div>
+            <a href="#" class="btn btn-primary btn-lg d-flex align-items-center justify-content-center text-uppercase rounded-0" data-aos="fade-left" data-aos-anchor-placement="top-bottom">
+              See More
+            </a>
+          </div>
+        </div>  
+      </div>
+    </div>
+  </div>
+</section>
