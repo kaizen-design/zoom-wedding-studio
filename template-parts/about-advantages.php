28a5da6dc9c1d5e3c6f92566f8906612c9c6acb5
kaizen-design <denis@kaizen-design.ru>
2025-10-12 18:14:30 +0300

    About: advantages section created

---
+++ template-parts/about-advantages.php
@@ -0,0 +1,31 @@
+<section class="advantages-section">
+  <div class="container-xl">
+     <div class="row">
+        <div class="section-content col-lg-8 offset-lg-3 d-flex flex-column position-relative">
+          <h2 class="section-title mb-0 d-flex flex-column">
+            <small class="section-subtitle text-uppercase" data-aos="fade-left">ADVANTAGES</small>
+            <span data-aos="fade-left" data-aos-delay=150>Why Choose ZOOM WEDDING STUDIO?</span>
+          </h2>
+          <p class="mb-0" data-aos="fade-left" data-aos-delay=300>It's a known fact that many businesses often engage third-party photographers or videographers who may not meet the expected standards. This is why we founded ZOOM WEDDING STUDIO and have assembled a team of passionate professionals who are committed to capturing the essence of your special day.<br /><br />
+          Our mission is to immerse you in a hurricane of positive emotions as you relive your special day through our meticulously edited photos and videos. Every detail and 
+          emotion captured during your wedding will become invaluable treasures within your lifelong memories.<br /><br />
+          We at ZOOM WEDDING STUDIO are grateful for your interest in our work. If you still have questions or are ready to start planning your wedding with us, feel free to get in touch via the form below.</p>
+          <a href="#" class="btn btn-primary btn-lg d-flex align-items-center align-self-start" data-aos="fade-left">
+            Request Pricing
+            <svg class="svg-arrow-right" width="15" height="15" viewBox="0 0 24 24" fill="none">
+              <path d="M16.3153 16.6681C15.9247 17.0587 15.9247 17.6918 16.3153 18.0824C16.7058 18.4729 17.339 18.4729 17.7295 18.0824L22.3951 13.4168C23.1761 12.6357 23.1761 11.3694 22.3951 10.5883L17.7266 5.9199C17.3361 5.52938 16.703 5.52938 16.3124 5.91991C15.9219 6.31043 15.9219 6.9436 16.3124 7.33412L19.9785 11.0002L2 11.0002C1.44772 11.0002 1 11.4479 1 12.0002C1 12.5524 1.44772 13.0002 2 13.0002L19.9832 13.0002L16.3153 16.6681Z" fill="#0F0F0F"/>
+            </svg>
+          </a>
+          <img 
+            src="<?= get_template_directory_uri() ?>/assets/tmp/advantages.webp" 
+            class="section-image img-fluid" 
+            width="535"
+            height="648"
+            alt="Advanteges"
+            loading="lazy"
+            data-aos="fade-right"
+          />
+        </div>
+     </div>
+  </div>
+</section>
